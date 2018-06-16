520f4b2a40e5cf7253cffe376285703ff04d0046
aihe <akhercha@student.42.fr>
2018-06-16 06:22:48 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # game-of-life
 My version of Conway's Game of Life.
+
+## I will write more here soon, I promise.
---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+function setup()
+{
+	createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+	background("#AACCFF");
+}
+
+function draw()
+{
+	fill(0);
+	noStroke();
+	rect(50, 50, 10, 10);
+}
