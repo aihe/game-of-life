21dccfdbf766c465de9429394a6445ec5de09ae8
aihe <akhercha@student.42.fr>
2018-06-16 07:24:46 +0200

    grid plan

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,42 @@
-let cell = new Cell(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+/*
+	CREATION GRILLE :
+
+	let arr = new Array(CANVAS_HEIGHT / 10);
+
+	for (let i = 0; i < arr.length; i++)
+	{
+		arr[i] = new Array(CANVAS_WIDTH / 10);
+	}
+
+	INSTANCIATION VALEURS GRILLE :
+
+	for (let j = 0; j < arr.length; j++)
+	{
+		for (let i = 0; i < arr[j].length; i++)
+		{
+			arr[j][i] = new Cell(i * 10, j * 10);
+			if (i % 2 == 0)
+			{
+				arr[j][i].is_alive = false;
+			}
+		}
+	}
+
+	AFFICHAGE GRILLE :
+
+	for (let j = 0; j < arr.length; j++)
+	{
+		for (let i = 0; i < arr[j].length; i++)
+		{
+			arr[j][i].display();
+		}
+	}
+*/
 
 function setup()
 {
 	createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
 	background(BACKGROUND_COLOR);
-	cell.display();
 }
 
 function draw()
